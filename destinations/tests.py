34178317f4dc8714e683eb8cef0822bbfd80952f
abhishek yepuru <>
2024-03-28 21:58:02 +0530

    added unit test cases
    as now i have checked unittest cases for field values and constrains

--- destinations/tests.py
+++ destinations/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from .models import Destination
 
-# Create your tests here.
+class Destinations_Unit_Test(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Destination.objects.create(
+            name='abhishek',
+            country='india',
+            description='all citeis in india',
+            best_time_to_visit='summer',
+            category='City',
+            image_url='http://examples1.com/test_image.jpg'
+        )
+
+    def test_name_max_length(self):
+        destination = Destination.objects.last()
+        max_length = destination._meta.get_field('name').max_length
+        self.assertEqual(max_length, 256)
+
+    def test_country_max_length(self):
+        destination = Destination.objects.last()
+        max_length = destination._meta.get_field('country').max_length
+        self.assertEqual(max_length, 100)
+
+    def test_category_choices(self):
+        destination = Destination.objects.last()
+        valid_choices = ['Beach', 'Mountain', 'City', 'Historical']
+        self.assertIn(destination.category, valid_choices)
+    def test_best_time_to_visit_max_length(self):
+        destination = Destination.objects.last()
+        max_length = len(destination.best_time_to_visit)
+        self.assertEqual(max_length, 100)
+        # self.assertLess(max_length, 100)
